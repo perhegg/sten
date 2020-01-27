7177475418393f8d1293ae8ce48ff93dba88e0bf
Per Eriksson <per_hegg@hotmail.com>
2020-01-27 15:13:27 +0100

    version 2.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
 let player_score = 1
 let comp_score = 1
-let resultat = document.getElementById("outcome")
+let resultat = document.querySelector(".items--9")
 
-document.getElementById("p").addEventListener('click', function(){
+document.querySelector(".paper").addEventListener('click', function(){
     Dice("p")
 })
-document.getElementById("s").addEventListener('click', function(){
+document.querySelector(".scissors").addEventListener('click', function(){
     Dice("s")
 })
-document.getElementById("st").addEventListener('click', function(){
+document.querySelector(".rock").addEventListener('click', function(){
     Dice("r")
 })
-document.getElementById("restart").addEventListener('click', function(){
+
+document.querySelector(".restart").addEventListener('click', function(){
     location.reload()
 })
 
 function Won_or_lost() {
-    if (player_score === 11) alert("You've won!!!!")
-    if (comp_score === 11) alert("You're a loser!!!!")  
+    if (player_score === 2) {
+        document.querySelector(".popup").classList.add('popup--open');
+        document.querySelector(".popup__text").innerHTML = "You won!";
+    } else if (comp_score === 2) {
+        document.querySelector(".popup").classList.add('popup--open');
+        document.querySelector(".popup__text").innerHTML = "You lost!";
+    }
+}
+
+function addClassName() {
+    document.querySelector(".paper").classList.add('items--5--effect')
+    document.querySelector(".rock").classList.add('items--3--effect')
+    document.querySelector(".scissors").classList.add('items--4--effect')
+}
+
+function removeClassName(comp) {
+        if(comp === "p") {
+            setTimeout(function(){
+            document.querySelector(".paper").classList.remove('items--5--effect')
+            }, 2000)    
+        } else if(comp==="s") {
+            setTimeout(function(){
+                document.querySelector(".scissors").classList.remove('items--4--effect')
+                }, 2000)    
+        } else if(comp==="r") {
+            setTimeout(function(){
+                document.querySelector(".rock").classList.remove('items--3--effect')
+                }, 2000)    
+        } else if ("all") {
+            setTimeout(function(){
+                document.querySelector(".paper").classList.remove('items--5--effect')
+                document.querySelector(".scissors").classList.remove('items--4--effect')    
+                document.querySelector(".rock").classList.remove('items--3--effect')
+                }, 4000)    
+        }
 }
 
 function convertToWord(letter){
@@ -29,22 +63,28 @@ return "Scissor"
 
 function Tie(user, comp){
     console.log("Tie")
-    resultat.innerHTML = `${convertToWord(user)} ties ${convertToWord(comp)}`
+    setTimeout(function(){
+        resultat.innerHTML = `<div class="outcome">${convertToWord(user)} ties ${convertToWord(comp)}</div>`
+    }, 2000)
 
 }
 
 function Win(user, comp){
     console.log("Win")
-    document.getElementById("player-score").innerHTML = player_score++
-    resultat.innerHTML = `${convertToWord(user)} beats ${convertToWord(comp)}`
-    Won_or_lost()
+    setTimeout(function(){
+        document.querySelector(".player--score").innerHTML = player_score++
+        resultat.innerHTML = `<div class="outcome">${convertToWord(user)} beats ${convertToWord(comp)}</div>`
+        Won_or_lost()
+    }, 2500)
 }
 
 function Lose(user, comp){
     console.log("Lose")
-    document.getElementById("computer-score").innerHTML = comp_score++
-    resultat.innerHTML = `${convertToWord(user)} loses to ${convertToWord(comp)}`
-    Won_or_lost()
+    setTimeout(function(){
+        document.querySelector(".computer--score").innerHTML = comp_score++
+        resultat.innerHTML = `<div class="outcome"> ${convertToWord(user)} loses to ${convertToWord(comp)}</div>`
+        Won_or_lost()
+    }, 2500)
 }
 
 function getNumber(){
@@ -73,5 +113,9 @@ function Dice(user) {
         Lose(user, comp)
         break;
     }
+
+    addClassName()
+    removeClassName(comp)
+    removeClassName("all")
 }
     
